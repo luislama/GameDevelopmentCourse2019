39a0745efa207aedf57d49aa4136bf62923753ca
Sunjing Lama <luislama@espol.edu.ec>
2019-06-09 19:59:59 -0500

    [ADD] monster runs from side to side

---
+++ Virtual Player Prototype/Assets/Code/MaceScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaceScript : MonoBehaviour
+{
+    bool toRight;
+    bool toLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Transform>().position = new Vector3(-23.0f, 0.0f, 0.0f);
+        toRight = true;
+        toLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toRight)
+        {
+            if (gameObject.transform.position.x < 19.75f)
+            {
+                gameObject.transform.Translate(10.0f*Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                linearStop();
+                toRight = false;
+                toLeft = true;
+            }
+        }
+        if (toLeft)
+        {
+            if (gameObject.transform.position.x > -19.85f )
+            {
+                gameObject.transform.Translate(-10.0f*Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                linearStop();
+                toLeft = false;
+                toRight = true;
+            }
+        }
+    }
+
+    private void linearStop()
+    {
+        var v = gameObject.GetComponent<Rigidbody2D>().velocity;
+        v =  Vector3.zero;
+        gameObject.GetComponent<Rigidbody2D>().velocity = v;
+    }
+}
